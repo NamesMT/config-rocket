3675fc150fcbfaeaaf0605de9af7f30998e74b1b
NamesMT <dangquoctrung123@gmail.com>
2025-04-26 10:11:49 +0000

    feat(unpack): ability to configure non-rocket archive behavior

--- src/cli/unpack.ts
+++ src/cli/unpack.ts
@@ -4,6 +4,7 @@ import type { FileOutputHooks } from '~/helpers/fs'
 import type { RocketAssembleHooks } from '~/rocket/assemble'
 import { rm } from 'node:fs/promises'
 import { join } from 'node:path'
+import consola from 'consola'
 import { strFromU8, unzip } from 'fflate'
 import { fileOutput } from '~/helpers/fs'
 import { logger } from '~/helpers/logger'
@@ -21,49 +22,80 @@ export interface UnpackOptions {
    * A hookable instance to hook into the rocket unpack (and related) process.
    */
   hookable?: Hookable<RocketUnpackHooks & RocketAssembleHooks & FileOutputHooks>
+
+  /**
+   * Control the behavior when the downloaded archive is not a rocket config pack.
+   * `true`: Continue extract anyway.
+   * `false`: Will abort the process.
+   * `prompt`: The user will be prompted to continue or abort.
+   *
+   * @default 'prompt'
+   */
+  nonAssemblyBehavior?: 'prompt' | true | false
 }
 
 export async function unpackFromUrl(url: string, options?: UnpackOptions) {
   const {
     hookable,
+    nonAssemblyBehavior = 'prompt',
   } = options ?? {}
 
-  logger.info(`Downloading config pack from ${url}`)
+  logger.info(`Downloading archive from ${url}`)
 
   const res = await fetch(url)
   if (!res.ok)
-    throw new Error(`Failed to download config pack from ${url}`)
+    throw new Error(`Failed to download archive from ${url}`)
 
   const configPackBuffer = new Uint8Array(await res.arrayBuffer())
 
-  logger.start('Extracting config pack to `.tmp`...')
+  logger.start('Extracting archive (to `.tmp` if needed)...')
+  let isRocketAssembly = true
+  let nonAssemblyContinue = typeof nonAssemblyBehavior === 'boolean' ? nonAssemblyBehavior : null
   await new Promise<void>((resolvePromise, rejectPromise) => {
     unzip(configPackBuffer, async (err, unzipped) => {
       if (err)
-        return rejectPromise(new Error('Failed to extract the config pack.'))
+        return rejectPromise(new Error('Failed to extract the archive.'))
 
       if (hookable)
         hookable.callHook('onExtract', { err, unzipped })
 
-      if (!unzipped['rocket.config.json5'])
-        return rejectPromise(new Error('Invalid config pack: "rocket.config.json5" not found.'))
+      if (!unzipped['rocket.config.json5']) {
+        isRocketAssembly = false
 
-      for (const [key, value] of Object.entries(unzipped))
-        await fileOutput(join('.tmp', key), strFromU8(value), { hookable })
+        if (typeof nonAssemblyContinue !== 'boolean' && nonAssemblyBehavior === 'prompt') {
+          nonAssemblyContinue = await consola.prompt(
+            'Archive is not a valid rocket config pack, do you want to continue extract anyway?',
+            { type: 'confirm', cancel: 'null' },
+          )
+        }
+
+        if (!nonAssemblyContinue)
+          return rejectPromise(new Error('Invalid config pack: "rocket.config.json5" not found.'))
+      }
+
+      for (const [key, value] of Object.entries(unzipped)) {
+        await fileOutput(
+          isRocketAssembly ? join('.tmp', key) : key,
+          strFromU8(value),
+          { hookable },
+        )
+      }
 
       resolvePromise()
     })
   })
   logger.success('Extracted successfully.')
 
-  logger.start('Assembling the config according to `rocketConfig`...')
-  await rocketAssemble({
-    frameDir: join('.tmp', 'frame'),
-    fuelDir: join('.tmp', 'fuel'),
-    outDir: '.',
-    hookable,
-  })
-  logger.start('Assembled successfully, removing temporary files...')
-  await rm('.tmp', { recursive: true })
-  logger.success('All done, enjoy your new config!')
+  if (isRocketAssembly) {
+    logger.start('Assembling the config according to `rocketConfig`...')
+    await rocketAssemble({
+      frameDir: join('.tmp', 'frame'),
+      fuelDir: join('.tmp', 'fuel'),
+      outDir: '.',
+      hookable,
+    })
+    logger.start('Assembled successfully, removing temporary files...')
+    await rm('.tmp', { recursive: true })
+    logger.success('All done, enjoy your new config!')
+  }
 }
