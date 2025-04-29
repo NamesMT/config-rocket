657b2b6727daa8c158aae5382c443fdc61e0d1b6
NamesMT <dangquoctrung123@gmail.com>
2025-04-29 09:00:26 +0000

    chore: rely on auto inference instead

--- build.config.ts
+++ build.config.ts
@@ -2,19 +2,6 @@ import { defineBuildConfig } from 'unbuild'
 import sharedConfig from './shared.config'
 
 export default defineBuildConfig({
-  entries: [
-    // Main engine
-    'src/index',
-
-    // CLI-specific exports
-    'src/cli',
-
-    // CLI app
-    'src/cli-entry',
-
-    // CLI `rocket-zip` app
-    'src/cli-rocket-zip-entry',
-  ],
   declaration: 'node16',
   clean: true,
   rollup: {
